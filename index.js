40380b6f91db3357f4ee68ab922dcf7b12d2b1b3
Mahede-Hasan <mahedehasan.cd91@gmail.com>
2022-10-06 01:28:04 -0700

    added

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors-service').collection('treatmentService');
+        const bookingCollection = client.db('doctors-service').collection('booking');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -23,6 +24,40 @@ async function run() {
             const service = await cursor.toArray();
             res.send(service);
         })
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+
+            const query = { date: date }
+            const bookings = await bookingCollection.find(query).toArray();
+            const services = await serviceCollection.find().toArray();
+
+            services.forEach(service=>{
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                const bookSlot = serviceBookings.map(book => book.slot)
+                const available = service.slots.filter(slot => !bookSlot.includes(slot))
+                service.slots = available
+            })
+            res.send(services);
+        })
+
+        app.get('/booking', async(req, res) =>{
+            const patient = req.query.patient;
+            const query = {patient: patient};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+          })
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+              return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+          })
     }
     finally {
 
